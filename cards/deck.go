9f2f9ee803c90cfd4da1476229350a1218ca85ef
Nigel Reoch <nigel.reoch@bellmedia.ca>
2018-07-12 16:42:32 -0400

    Added more deck functions

--- cards/deck.go
+++ cards/deck.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
 )
 
 // Create a new type of deck
@@ -14,14 +17,34 @@ func newDeck() deck {
 	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
 	for _, suit := range cardSuits {
 		for _, value := range cardValues {
-			cards = append(cards, value+" "+suit)
+			cards = append(cards, value+" of "+suit)
 		}
 	}
 	return cards
 }
 
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
+	}
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
 	}
 }
