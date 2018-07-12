9953655c233eaffbff8a67817622ea63534de0ff
Nigel Reoch <nigel.reoch@bellmedia.ca>
2018-07-12 12:28:21 -0400

    Create a full deck

--- cards/main.go
+++ cards/main.go
@@ -1,12 +1,6 @@
 package main
 
 func main() {
-	cards := deck{"Ace of Spades", "Ace of Diamonds"}
-	// append returns a new slice of cards with the new value
-	cards = append(cards, "Two of Spades")
+	cards := newDeck()
 	cards.print()
-}
-
-func newCard() string {
-	return "Ace of Spades"
 }
