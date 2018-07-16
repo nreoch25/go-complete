ffa4dd08bfaf53c297d1a3e4ee2e1fdb09f1414e
Nigel Reoch <nigel.reoch@bellmedia.ca>
2018-07-16 12:35:05 -0400

    Continued structs

--- structs/main.go
+++ structs/main.go
@@ -1,8 +1,36 @@
 package main
 
+import "fmt"
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contactInfo
 }
 
-func main() {}
+func main() {
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Koestler",
+		contactInfo: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 94000,
+		},
+	}
+	jimPointer := &jim
+	jimPointer.updateName("Jimmy")
+	jim.print()
+}
+
+func (pointerToPerson *person) updateName(newFirstName string) {
+	pointerToPerson.firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v\n", p)
+}
